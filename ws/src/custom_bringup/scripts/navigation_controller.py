39e3e4d45e4f32aa2950f9a52532a1eb5db98452
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2025-12-17 01:20:09 +0800

    .

--- ws/src/custom_bringup/scripts/navigation_controller.py
+++ ws/src/custom_bringup/scripts/navigation_controller.py
@@ -98,10 +98,10 @@ class NavigationController:
         #compute average normal vector
         normal_vec_sum = Vector2(0,0)
         for vec in average_inlier_vec:
-            normal_vec_sum.add(vec)
+            normal_vec_sum.add()
             average_normal_vec_median = normal_vec_sum.normalize()
 
-        normal_vec_median = average_normal_vec_median
+        normal_vec_median = average_normal_vec_median.normal()
 
         res = self.local_map_msg.info.resolution
 
