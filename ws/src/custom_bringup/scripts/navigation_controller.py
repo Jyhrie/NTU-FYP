0fb3af270c92eb847ed7bda6c5ab7d485d114e37
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2025-12-20 01:06:47 +0800

    .

--- ws/src/custom_bringup/scripts/navigation_controller.py
+++ ws/src/custom_bringup/scripts/navigation_controller.py
@@ -225,7 +225,7 @@ class NavigationController:
             self.enqueue(Command(CommandType.TURN, target_yaw=target_yaw))
             if median_outlier is not None:
                 stop_vec = Vector2(cx - median_outlier.x + ((normal_vec_median.x * HUG_DISTANCE) / res) - ((average_wall_vec_median.x * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res),
-                                    cy - median_outlier.y + ((normal_vec_median.y * HUG_DISTANCE) / res) - ((-average_wall_vec_median.y * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res)) #need to flip the sign of average_wall_vec median for it to not overshoot since -Y is forward
+                                    cy - median_outlier.y + ((normal_vec_median.y * HUG_DISTANCE) / res) - ((average_wall_vec_median.y * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res)) #need to flip the sign of average_wall_vec median for it to not overshoot since -Y is forward
                 
                 print("Stop Vec: ", stop_vec)
                 if(stop_vec.y < 0):
@@ -238,7 +238,7 @@ class NavigationController:
                 print("Last Inlier: ", last_inlier)
                 print("average_wall_vec_median: ", average_wall_vec_median)
                 stop_vec = Vector2(cx - last_inlier.x + ((normal_vec_median.x * HUG_DISTANCE) / res) - ((average_wall_vec_median.x * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res),
-                                    cy - last_inlier.y + ((normal_vec_median.y * HUG_DISTANCE) / res) - ((-average_wall_vec_median.y * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res))
+                                    cy - last_inlier.y + ((normal_vec_median.y * HUG_DISTANCE) / res) - ((average_wall_vec_median.y * (TURN_SAFE_DISTANCE + ROBOT_SAFE_SQUARE_FOOTPRINT)) / res))
                 mag_dist_to_stop_point = stop_vec.mag() * res
 
                 print("Stop Vec: ", stop_vec)
