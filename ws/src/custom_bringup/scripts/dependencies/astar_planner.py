16b1469f1e3e759d2f55b534bbc0d4865ef488e1
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-02-20 22:42:55 +0800

    .

--- ws/src/custom_bringup/scripts/dependencies/astar_planner.py
+++ ws/src/custom_bringup/scripts/dependencies/astar_planner.py
@@ -92,7 +92,7 @@ def a_star_exploration(static_map_raw, costmap_raw, start, goal,
             # We treat the costmap as an additive penalty. 
             # 1000 * cost means the robot would rather walk 50 meters in a 
             # clear hallway than 1 meter near a wall.
-            traversal_cost = move_dist + (COSTMAP_WEIGHT * (cm[ny, nx] / 100))
+            traversal_cost = move_dist + (COSTMAP_WEIGHT * (cm[ny, nx]))
             tg = g + traversal_cost
 
             if tg < g_score[ny, nx]:
