c44dc45168c3b6d006fc16471ca024b03118e0e7
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-02-20 19:37:10 +0800

    .

--- ws/src/custom_bringup/scripts/frontier_node.py
+++ ws/src/custom_bringup/scripts/frontier_node.py
@@ -135,7 +135,7 @@ class FrontierNode:
                         math.cos(first_step_angle - yaw)
                     )
                     if abs(angle_diff) > math.radians(90):
-                        print(f"First move requires {math.degrees(angle_diff):.1f}° turn, rotating first.")
+                        print(f"First move requires {math.degrees(angle_diff):.1f}deg turn, rotating first.")
                         self.publish_rotate_command()
                         return
 
