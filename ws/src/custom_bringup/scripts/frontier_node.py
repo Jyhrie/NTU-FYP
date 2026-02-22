a59f8a789c8ae4b03308390438a954e377c2f593
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-02-22 15:49:38 +0800

    .

--- ws/src/custom_bringup/scripts/frontier_node.py
+++ ws/src/custom_bringup/scripts/frontier_node.py
@@ -138,7 +138,7 @@ class FrontierNode:
                 self.map.data, self.global_costmap, start, frontier
             )
 
-            if len(path) < TRUNCATION_SIZE+1:
+            if len(path) < TRUNCATION_SIZE+5:
                 print("Discarding path to frontier {} due to insufficient length: {}".format(frontier, len(path)))
                 continue
 
