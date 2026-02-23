13e1c04bd5e21d419b3f5ba99024fd073e48e42f
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-02-23 11:20:50 +0800

    .

--- arm_test.py
+++ arm_test.py
@@ -62,7 +62,7 @@ def move_arm():
         arm_msg = Arm()
         j8 = Joint()
         j8.id = 8
-        j8.angle = 140 # Middle position #30 is TUCKED, 180 is MAX
+        j8.angle = 180 # Middle position #30 is TUCKED, 180 is MAX
         j8.run_time = 1000 
 
         arm_msg.joint.append(j8)
