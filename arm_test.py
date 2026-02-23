270911d7534ca376db3155f73fd8fd7cd1cb7742
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-02-23 11:06:58 +0800

    .

--- arm_test.py
+++ arm_test.py
@@ -46,7 +46,7 @@ def move_arm():
         # Joint 1: Lift the main arm (ID 7)
         j7 = Joint()
         j7.id = 7
-        j7.angle = 35 # Middle position
+        j7.angle = 80 # Middle position
         j7.run_time = 1000 
 
         #220 is TUCKED
@@ -61,7 +61,7 @@ def move_arm():
         # Joint 2: Close the claw (ID 9)
         j9 = Joint()
         j9.id = 9
-        j9.angle = 50 # 180 degrees (Closed tight)
+        j9.angle = 180 # 180 degrees (Closed tight)
         j9.run_time = 1000
 
         multi_msg.joint = [j7, j8, j9] # Add both to the list
