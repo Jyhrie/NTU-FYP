9404a96c28159e0eb01590f7ad23293eb16a7f2d
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-03-08 21:59:16 +0800

    .

--- ws/src/custom_bringup/scripts/map_manager.py
+++ ws/src/custom_bringup/scripts/map_manager.py
@@ -276,7 +276,7 @@ class PathingNode:
             goal_cell = (safe_gx, safe_gy)
 
             rospy.loginfo("Planning path to safe spot near object: grid({}, {})".format(safe_gx, safe_gy))
-            self.publish_marker(goal_cell, color="blue")
+            self.publish_marker(safe_gx, safe_gy, color="blue")
             
             path, success = a_star_exploration(
                 self.map.data, self.global_costmap, start_cell, goal_cell
