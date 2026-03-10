adb4fa648cd35b079aaf148a5013dfe7e4e4ff56
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-03-10 23:36:42 +0800

    .

--- ws/src/custom_bringup/scripts/robot_controller.py
+++ ws/src/custom_bringup/scripts/robot_controller.py
@@ -466,7 +466,7 @@ class Controller:
                 "header": "movement",
                 "command": "approach",
                 "extra": "face_coordinates",
-                "stopping_distance": 0.70,
+                "stopping_distance": 1.2,
                 "x": obj_x,
                 "y": obj_y
             })
