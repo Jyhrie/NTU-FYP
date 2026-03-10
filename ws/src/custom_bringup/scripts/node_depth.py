9d7a95908277edcd03f09f73082b88a1124e6878
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-03-10 23:57:25 +0800

    .

--- ws/src/custom_bringup/scripts/node_depth.py
+++ ws/src/custom_bringup/scripts/node_depth.py
@@ -11,7 +11,8 @@ INPUT_W         = 640
 INPUT_H         = 640
 ASTRA_PRO_HFOV  = 58.4
 ANGLE_OFFSET = 3
-class BlobCentroidEstimator:
+ASSUMED_DEPTH_OFFSET = 0.02
+class BlobCentroidEstimator: 
     def __init__(self):
         rospy.init_node('blob_distance_node', anonymous=True)
         
@@ -86,7 +87,7 @@ class BlobCentroidEstimator:
                 "header": "depth_reading",
                 "x": round(global_x, 1),
                 "y": round(global_y, 1),
-                "dist_m": round(exact_dist_mm / 1000.0, 3),
+                "dist_m": round(exact_dist_mm / 1000.0, 3) + ASSUMED_DEPTH_OFFSET,
                 "angle_deg": -round(angle_deg, 2)  #- ANGLE_OFFSET
             })
             self.depth_pub.publish(result)
