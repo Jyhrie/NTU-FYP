bd4462464fe19842cbc86f8597bd90d546413d61
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-04-05 15:00:24 +0800

    .

--- ws/src/custom_bringup/scripts/robot_controller.py
+++ ws/src/custom_bringup/scripts/robot_controller.py
@@ -556,6 +556,7 @@ class Controller:
 
             
         if self.sub_state == SubStates.APPROACH_ITEM:
+            rospy.sleep(1.5)
             obj_x, obj_y = self.target_object_transform
             msg = String()
             msg.data = json.dumps({
