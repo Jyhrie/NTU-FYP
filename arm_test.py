d54b4ee6486a10419677b4cc974826d681e0fea2
Jyhrie <104437222+Jyhrie@users.noreply.github.com>
2026-05-04 23:09:17 +0800

    fk new

--- arm_test.py
+++ arm_test.py
@@ -35,25 +35,25 @@ def move_arm_staggered():
         rospy.sleep(2.0) # Wait for full extension
 
         # --- STEP 2: GRIP ---
-        send_single_joint(9, 100, 800)
+        send_single_joint(9, 110, 800)
         rospy.sleep(1.0)
 
         # --- STEP 3: HALF TUCK (holding item)---
         # Stagger the return
         send_single_joint(7, 220, 2000)
         send_single_joint(8, 50, 2000)
-        send_single_joint(9, 85, 1000)
+        send_single_joint(9, 110, 1000)
         rospy.sleep(2.5)
 
         # --- STEP : (RELEASE item)---
         # send_single_joint(9, 30, 800)
-        send_single_joint(8, 60, 2000)
+        send_single_joint(8, 30, 2000)
         rospy.sleep(2.5)
         # --- STEP 3: FULL TUCK ---
         # Stagger the return
         send_single_joint(7, 220, 2000)
         send_single_joint(8, 30, 2000)
-        send_single_joint(9, 85, 1000)
+        send_single_joint(9, 30, 1000)
         rospy.sleep(2.5)
 
     except rospy.ROSInterruptException:
